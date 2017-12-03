7e86c068dd77d165a3a94b6ee7d0e8b28c99af0a
JafarAkhondali <jafar.akhondali@yahoo.com>
2017-12-03 03:38:51 +0330

    Fancier installer

--- install.sh
+++ install.sh
@@ -1,25 +1,53 @@
 #/bin/bash
 
+function program_is_installed {
+  # set to 1 initially
+  local return_=1
+  # set to 0 if not found
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  # return value
+  echo "$return_"
+}
+
+# display a message in red with a cross by it
+# example
+# echo echo_fail "No"
+function echo_fail {
+  # echo first argument in red
+  printf "\e[31m✘ ${1}"
+  # reset colours back to normal
+  tput sgr0
+}
+
+# display a message in green with a tick by it
+# example
+# echo echo_fail "Yes"
+function echo_pass {
+  # echo first argument in green
+  printf "\e[32m✔ ${1}"
+  # reset colours back to normal
+  tput sgr0
+}
+
+function echo_if {
+  if [ $1 == 1 ]; then
+    echo_pass $2
+  else
+    echo_fail $2
+    canInstall="no"
+  fi
+}
+
 canInstall="yes"
-if [ -x /usr/bin/pngquant ]; then
-  echo "pngquant is not installed !"
-  canInstall="no"
-fi
 
-if [ -x /usr/bin/parallel ]; then
-  echo "parallel is not installed !"
-  canInstall="no"
-fi
 
-if [ -x /usr/bin/jpegoptim ]; then
-  echo "pngquant is not installed !"
-  canInstall="no"
-fi
+echo "pngquant    $(echo_if $(program_is_installed pngquant))"
+echo "parallel    $(echo_if $(program_is_installed parallel))"
+echo "jpegoptim    $(echo_if $(program_is_installed jpegoptim))"
 
 if [[ $canInstall == "no" ]]; then
   exit -1
 fi
-
 
 git clone https://github.com/JafarAkhondali/Tapnesh.git /tmp/tapnesh
 cd /tmp/tapnesh
